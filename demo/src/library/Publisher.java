748d4328cb6cabea083f3a711118c60775e5da12
srikanthpragada <srikanthpragada@gmail.com>
2023-03-07 19:56:48 +0530

    07-MAR

--- demo/src/library/Publisher.java
+++ demo/src/library/Publisher.java
@@ -1,9 +1,10 @@
 package library;
 
+// package access 
 class Title {
 
 }
 
 public class Publisher {
-
+   private Title title;
 }
---
+++ demo/src/test/TestLibrary.java
@@ -0,0 +1,13 @@
+package test;
+
+import library.*;
+
+public class TestLibrary {
+
+	public static void main(String[] args) {
+		Book b;
+		library.Publisher p; // Fully Qualified Classname 
+
+	}
+
+}
