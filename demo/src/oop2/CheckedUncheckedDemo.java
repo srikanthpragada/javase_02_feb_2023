a7aefe6bf48fe5a8f1679c7f853a42fa7a0b4b72
srikanthpragada <srikanthpragada@gmail.com>
2023-03-09 19:54:52 +0530

    09-MAR

---
+++ demo/src/oop2/CheckedUncheckedDemo.java
@@ -0,0 +1,13 @@
+package oop2;
+
+public class CheckedUncheckedDemo {
+
+	public static void main(String[] args) {
+		int v = Integer.parseInt("33434");
+		
+		//Thread.sleep(1000);
+		
+
+	}
+
+}
--- demo/src/oop2/ExDemo.java
+++ demo/src/oop2/ExDemo.java
@@ -3,13 +3,15 @@ package oop2;
 public class ExDemo {
 
 	public static void main(String[] args) {
-		String st = "124";
+		String st = "0";
 
 		try {
 			int v = Integer.parseInt(st);
-			System.out.println(v);
+			System.out.println(100 / v);
 		} catch (NumberFormatException ex) {
 			System.out.println("Sorry! Invalid number!");
+		} catch (ArithmeticException ex) {
+			System.out.println("Sorry! Zero is not valid!");
 		}
 
 		System.out.println("THe End!");
