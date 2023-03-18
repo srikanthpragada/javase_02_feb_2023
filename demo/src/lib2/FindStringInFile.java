ea1b8f24f2b27003ddb0d68e62182852b1a645d0
srikanthpragada <srikanthpragada@gmail.com>
2023-03-18 19:56:44 +0530

    18-MAR

--- demo/src/lib2/FindStringInFile.java
+++ demo/src/lib2/FindStringInFile.java
@@ -1,6 +1,5 @@
 package lib2;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -9,7 +8,7 @@ public class FindStringInFile {
 	public static void main(String[] args) throws Exception {
 		String search = "test";
 
-		Path p = Path.of("d:\\classroom\\feb2j\\test.txt");
+		Path p = Path.of("d:\\classroom\\feb2j\\test.txt");  // WindowsPath
 		System.out.println(p.getClass());
 		var contents = Files.readString(p);
 
